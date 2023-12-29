41db449acde471e42c5adb8866f85e2d3fe8a649
sosound <clearputaoshu@163.com>
2023-12-30 00:44:25 +0800

    add django-sslserver

--- django_auth/settings_cloud.py
+++ django_auth/settings_cloud.py
@@ -53,7 +53,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app01',
-    'corsheaders'
+    'corsheaders',
+    'sslserver'
 ]
 
 MIDDLEWARE = [
--- manage_cloud.py
+++ manage_cloud.py
@@ -2,15 +2,6 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import ssl
-
-from django.core.management import execute_from_command_line
-
-CERTIFICATE_PATH = './certs/galaxystream_online_ssl.crt'
-PRIVATE_KEY_PATH = './certs/galaxystream_online_ssl.key'
-
-ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
-ssl_context.load_cert_chain(CERTIFICATE_PATH, PRIVATE_KEY_PATH)
 
 
 def main():
@@ -24,7 +15,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv, ssl_context=ssl_context)
+    execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
