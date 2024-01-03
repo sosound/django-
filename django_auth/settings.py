a1a6c678a5109503072f7e9e0c1d455c44aab5c1
sosound <clearputaoshu@163.com>
2024-01-04 00:06:49 +0800

    c

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'django_auth.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'static']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
